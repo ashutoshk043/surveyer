cbdebd199076e852546fc19ddf4f57b859fbb3bd
Ashutosh Kumar <ashutoshk043@gmail.com>
2025-05-16 01:26:57 +0530

    added cards stattic

--- src/app/features/home/home/home.component.ts
+++ src/app/features/home/home/home.component.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone:true,
   imports: [MatSlideToggleModule, CardComponent, CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
@@ -15,32 +16,44 @@ export class HomeComponent {
     {
       title: 'Card 1',
       image: 'https://cdn.prod.website-files.com/59e16042ec229e00016d3a66/5e62f6f4dad6467a6ba6a589_pie%20sample%203.webp',
-      description: 'This is the first card.'
+      description: 'This is the first card.',
+      tag:"",
+      author:""
     },
     {
       title: 'Card 2',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQoJVaTGo1WYJ3JrQQzMuJ1s9lRQuuruDVpg&s',
-      description: 'This is the second card.'
+      description: 'This is the second card.',
+      tag:"",
+      author:""
     },
     {
       title: 'Card 1',
       image: 'https://cdn.prod.website-files.com/59e16042ec229e00016d3a66/5e62f6f4dad6467a6ba6a589_pie%20sample%203.webp',
-      description: 'This is the first card.'
+      description: 'This is the first card.',
+      tag:"",
+      author:""
     },
     {
       title: 'Card 2',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQoJVaTGo1WYJ3JrQQzMuJ1s9lRQuuruDVpg&s',
-      description: 'This is the second card.'
+      description: 'This is the second card.',
+      tag:"",
+      author:""
     },
     {
       title: 'Card 1',
       image: 'https://cdn.prod.website-files.com/59e16042ec229e00016d3a66/5e62f6f4dad6467a6ba6a589_pie%20sample%203.webp',
-      description: 'This is the first card.'
+      description: 'This is the first card.',
+      tag:"",
+      author:""
     },
     {
       title: 'Card 2',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQoJVaTGo1WYJ3JrQQzMuJ1s9lRQuuruDVpg&s',
-      description: 'This is the second card.'
+      description: 'This is the second card.',
+      tag:"",
+      author:""
     }
   ];
   
